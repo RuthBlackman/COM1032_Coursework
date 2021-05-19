7ec8ccdd2ab535f1cde6ed348919b0bff6b7539a
Ruth Blackman <rkjb410@gmail.com>
2021-05-19 03:03:21 +0100

    Sorted output

--- src/urn6623139/Simulation.java
+++ src/urn6623139/Simulation.java
@@ -23,49 +23,75 @@ public class Simulation {
 		 * Example A.1.2
 		 */
 		
-		/*
-		System.out.println("Start Example A.1.2\n");
+		System.out.println("\nStart Example A.1.2\n");
 		
 		String example1 = "1, 100, 100, 10";
 		memManager.parseExample(example1);
+		System.out.println("After step 1");
+		memManager.printMemory();
 		
 		String example2 = "2, 200";
 		memManager.parseExample(example2);
+		System.out.println("After step 2");
+		memManager.printMemory();
 		
 		String example3 = "1, -40, 0,0";
 		memManager.parseExample(example3);
+		System.out.println("After step 3");
+		memManager.printMemory();
 		
 		String example4 = "2, -10, 300";
 		memManager.parseExample(example4);
+		System.out.println("After step 4");
+		memManager.printMemory();
 		
-		String example5 = "3, 180";
+		String example5 = "3, 30";
 		memManager.parseExample(example5);
+		System.out.println("After step 5");
+		memManager.printMemory();
 		
-		System.out.println("\nEnd Example A.1.2");
-		*/
+		String example6 = "4, 100";
+		memManager.parseExample(example6);
+		System.out.println("After step 6");
+		memManager.printMemory();
+		
+		String example7 = "5, 20";
+		memManager.parseExample(example7);
+		System.out.println("After step 7");
+		memManager.printMemory();
+		
+		String example8 = "5, -15";
+		memManager.parseExample(example8);
+		System.out.println("After step 8");
+		memManager.printMemory();
+
+		System.out.println("End Example A.1.2");
+		System.out.println("\n\n---------------------------------------------------------------------------------------------------\n\n");
+		
 		
 		
 		/*
 		 * Example A.2.1
 		 */
 
-		/*
 		System.out.println("Start Code Location A.2.1 read-write protection");
-		System.out.println("...filename and line number...");
 		System.out.println("Filename: MemoryManagement.java");
-		System.out.println("Line number: ");
-		System.out.println("End Code Location A.2.1 read-write protection");	
+		System.out.println("Line number: 205");
+		System.out.println("Line number: 678");
+		System.out.println("End Code Location A.2.1 read-write protection\n");	
 		
-		System.out.println("Start Example A.2.1 read-write protection");
+		System.out.println("Start Example A.2.1 read-write protection\n");
 		MemoryManagement memManager2 = new MemoryManagement(1024, 124);
 		
-		String example6 = "1, [100; 0], [200; 1], 10"; //allocate memory for 3 segments in process 1
-		memManager2.parseExample(example6);
-		System.out.println("\n");
+		String example9 = "1, [100; 0], [200; 1], 10"; //allocate memory for 3 segments in process 1
+		memManager2.parseExample(example9);
 		memManager2.printReadWriteStatus();
+		String example21 = "1, [100;1], 0, 0";
+		memManager2.parseExample(example21);
+		memManager2.printReadWriteStatus();		
+		System.out.println("\nEnd Example A.2.1 read-write protection");	
+		System.out.println("\n\n---------------------------------------------------------------------------------------------------");
 		
-		System.out.println("End Example A.2.1 read-write protection");	
-		*/
 		
 
 				
@@ -74,7 +100,7 @@ public class Simulation {
 		 */
 		
 		/*
-		System.out.println("Start Code Location A.2.2 sharing of segments");
+		System.out.println("\n\nStart Code Location A.2.2 sharing of segments");
 		System.out.println("...filename and line number...");
 		System.out.println("End Code Location A.2.2 sharing of segments");
 		
@@ -136,50 +162,50 @@ public class Simulation {
 		 * Example A.3.1
 		 */
 		
-		/*
 		System.out.println("Start Code Location A.3.1 TLB");
-		System.out.println("...filename and line number...");
 		System.out.println("Filename: MemoryManagement.java");
-		System.out.println("Line number: ");
+		System.out.println("Line number: 722 ");
+		System.out.println("Line number: 740 ");
 		System.out.println("End Code Location A.3.1 TLB\n");
 		
 		System.out.println("Start Example A.3.1 TLB working");
-		String example3 = "1, [10; 1], [200; 0; 2; 5], 10";
-		memManager.parseExample(example3);
-		memManager.printTLBWorking();
+		MemoryManagement memManager3 = new MemoryManagement(1024, 124);
+		String example10 = "1, [10; 1], [200; 0; 2; 5], 10";
+		memManager3.parseExample(example10);
+		memManager3.printMemory();
+		memManager3.printTLBWorking();
 		System.out.println("End Example A.3.1 TLB working\n");
 		
 		System.out.println("Start Example A.3.1 TLB miss");
-		memManager.printTLBMiss();
+		memManager3.printTLBMiss();
 		System.out.println("End Example A.3.1 TLB miss");
-		/*
 		
+		System.out.println("\n\n---------------------------------------------------------------------------------------------------\n\n");
 
 		
 		/*
 		 * Example A.3.2
 		 */
 		
-		/*
 		System.out.println("Start Code Location A.3.2 Compaction");
-		//System.out.println("...filename and line number...");
 		System.out.println("Filename: MemoryManagement.java");
-		System.out.println("Line number: 539");
+		System.out.println("Line number: 573");
 		System.out.println("End Code Location A.3.2 Compaction\n");
 		
 		System.out.println("Start Example A.3.2 Compaction");
-		//System.out.println("...the example...");
-		String example4  = "1, 100, 200, 3";
-		memManager.parseExample(example4);
-		String example5 = "1, -25, -50, -1";
-		memManager.parseExample(example5);
-		System.out.println("Before compaction");
-		memManager.printMemory();
-		memManager.compaction();
+		MemoryManagement memManager4 = new MemoryManagement(1024, 124);
+		String example11  = "1, 100, 200, 3";
+		memManager4.parseExample(example11);
+		String example12 = "1, -25, -50, -1";
+		memManager4.parseExample(example12);
+		System.out.println("\nBefore compaction");
+		memManager4.printMemory();
+		memManager4.compaction();
 		System.out.println("After compaction");
-		memManager.printMemory();
+		memManager4.printMemory();
 		System.out.println("End Example A.3.2 Compaction");
-		*/
+		
+		System.out.println("\n\n---------------------------------------------------------------------------------------------------");
 	}
 	
 	
